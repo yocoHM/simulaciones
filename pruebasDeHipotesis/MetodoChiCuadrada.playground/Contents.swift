557bc18794a299dd28a5c2f132a72275e180f167
yocoHM <yocoyanihernandez@gmail.com>
2016-10-18 10:05:06 -0500

    ajuste automatico del intervalo

--- pruebasDeHipotesis/MetodoChiCuadrada.playground/Contents.swift
+++ pruebasDeHipotesis/MetodoChiCuadrada.playground/Contents.swift
@@ -17,41 +17,58 @@ if let path = filePath {
     
     print("N = \(numeros.count)")
     let K:Float = ceil(logf(Float(numeros.count)) / logf(2.0))
+    print("K = \(Int(K))")
     
     var intervalo = (numeros.max()! - numeros.min()!) / K
-    print("Intervalo = \(intervalo)")
-    
     
     let digitos = String(intervalo).characters.split{$0 == "."}.map(String.init)
     var decimales = digitos[1].characters.count
     let ajuste = 1 / (pow(10.0, Float(decimales)))
     
-    //************ Ajuste al intervalo(variable para modificar) ************
-    intervalo += 0.01; print("Nuevo intervalo = \(intervalo)")
-    
     var min = numeros.min()!
     var mayor = min + intervalo - ajuste
+    var total = 0
+    var conts:[Float] = Array()
+    var cont = 0
     
-    print("--- Intervalos ---")
-    for i in 1...Int(K) {
-      print("\(min) - \(mayor)")
-      min = mayor + ajuste
+    //en este repeat se ajusta el intervalo solito
+    repeat {
+      min = numeros.min()!
       mayor = min + intervalo - ajuste
-    }
-    min
-    if (numeros.max()! > min) {
-      print("REALIZAR AJUSTE en 'intervalo'")
-    }
-    else {
-      print("INTERVALOS ADECUADOS\n")
+      total = 0
+      conts = Array()
+      cont = 0
       
-      var conts:[Float] = Array()
+      for i in 1...Int(K) {
+        
+        for i in numeros {
+          if (i >= min && i <= mayor){
+            cont += 1
+          }
+        }
+        conts.append(Float(cont))
+        total += cont
+        
+        min = mayor + ajuste
+        mayor = min + intervalo - ajuste
+        cont = 0
+      }//cierre del for para obtener los conts
+      
+      if (total != numeros.count) {
+        intervalo += ajuste
+      }
+      
+    } while ((numeros.max()! > min) && total != numeros.count)
+    
+    if (total == numeros.count){
       
       min = numeros.min()!
       mayor = min + intervalo - ajuste
-      var cont = 0
-      var total = 0
+      total = 0
+      conts = Array()
+      cont = 0
       
+      print("\n--- Intervalos ---")
       for i in 1...Int(K) {
         print("\(min) - \(mayor)")
         
@@ -69,23 +86,19 @@ if let path = filePath {
         cont = 0
       }//cierre del for para obtener los conts
       
-      if (total == numeros.count){
-        print("La cantidad de numeros en cada intervalo es correcta\n")
-        
-        let Ei = (1.0/K) * 100.0
-        var X2:Float = 0.0
-        
-        for i in 0..<conts.count {
-          X2 += (pow(conts[i] - Ei, 2)) / Ei
-        }
-        
-        print("X2 = \(X2)")
+      let Ei = (1.0/K) * 100.0
+      var X2:Float = 0.0
+      
+      for i in 0..<conts.count {
+        X2 += (pow(conts[i] - Ei, 2)) / Ei
       }
-      else {
-        print("ERROR: Revisar la cantidad de numeros en cada intervalo")
-      }
-
-    }//cierre para else de intervalos adecuados
+      
+      print("\nX2 = \(X2)")
+    }
+    else {
+      print("ERROR: Revisar la cantidad de numeros en cada intervalo")
+    }
+    
   }//cierre de if cuando hay contenido
   else{
     print("No hay contenido que se pueda usar")
