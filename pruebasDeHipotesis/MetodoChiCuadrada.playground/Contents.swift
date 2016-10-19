a3d3df2da2027d038fae4f4163500d5492974ca5
yocoHM <yocoyanihernandez@gmail.com>
2016-10-18 20:22:30 -0500

    ajuste de intervalos para cuando tienen menos de 5 valores

--- pruebasDeHipotesis/MetodoChiCuadrada.playground/Contents.swift
+++ pruebasDeHipotesis/MetodoChiCuadrada.playground/Contents.swift
@@ -15,26 +15,27 @@ if let path = filePath {
     let numerosArregloStr = contenido.characters.split{$0 == ","}.map(String.init)
     var numeros:[Float] = numerosArregloStr.map { Float($0)! }
     
-    print("N = \(numeros.count)")
-    let K:Float = ceil(logf(Float(numeros.count)) / logf(2.0))
-    print("K = \(Int(K))")
+    var K:Float = ceil(logf(Float(numeros.count)) / logf(2.0))
     
     var intervalo = (numeros.max()! - numeros.min()!) / K
     
     let digitos = String(intervalo).characters.split{$0 == "."}.map(String.init)
     var decimales = digitos[1].characters.count
-    let ajuste = 1 / (pow(10.0, Float(decimales)))
+    let unidadMinima = 1 / (pow(10.0, Float(decimales)))
     
     var min = numeros.min()!
-    var mayor = min + intervalo - ajuste
+    var mayor = min + intervalo - unidadMinima
     var total = 0
     var conts:[Float] = Array()
+    var intervalos:[(Float,Float)] = Array()
     var cont = 0
+    var Ei:Float = 0.0
+    var X2:Float = 0.0
     
     //en este repeat se ajusta el intervalo solito
     repeat {
       min = numeros.min()!
-      mayor = min + intervalo - ajuste
+      mayor = min + intervalo - unidadMinima
       total = 0
       conts = Array()
       cont = 0
@@ -49,13 +50,13 @@ if let path = filePath {
         conts.append(Float(cont))
         total += cont
         
-        min = mayor + ajuste
-        mayor = min + intervalo - ajuste
+        min = mayor + unidadMinima
+        mayor = min + intervalo - unidadMinima
         cont = 0
       }//cierre del for para obtener los conts
       
       if (total != numeros.count) {
-        intervalo += ajuste
+        intervalo += unidadMinima
       }
       
     } while ((numeros.max()! > min) && total != numeros.count)
@@ -63,14 +64,14 @@ if let path = filePath {
     if (total == numeros.count){
       
       min = numeros.min()!
-      mayor = min + intervalo - ajuste
+      mayor = min + intervalo - unidadMinima
       total = 0
       conts = Array()
+      intervalos = Array()
+      
       cont = 0
       
-      print("\n--- Intervalos ---")
       for i in 1...Int(K) {
-        print("\(min) - \(mayor)")
         
         for i in numeros {
           if (i >= min && i <= mayor){
@@ -78,22 +79,84 @@ if let path = filePath {
           }
         }
         conts.append(Float(cont))
-        total += cont
-        print("Cantidad de nums en el intervalo = \(cont)")
         
-        min = mayor + ajuste
-        mayor = min + intervalo - ajuste
+        var tuplaIntervalos = (min,mayor)
+        intervalos.append(tuplaIntervalos)
+        
+        total += cont
+        
+        min = mayor + unidadMinima
+        mayor = min + intervalo - unidadMinima
         cont = 0
       }//cierre del for para obtener los conts
       
-      let Ei = (1.0/K) * 100.0
-      var X2:Float = 0.0
+      var menorEncontrado = false
+      var j:Int = 0
       
-      for i in 0..<conts.count {
-        X2 += (pow(conts[i] - Ei, 2)) / Ei
+      while (!menorEncontrado && j != conts.count){
+        if (conts[j] < 5){
+          menorEncontrado = true
+        }
+        j += 1
       }
       
-      print("\nX2 = \(X2)")
+      if (menorEncontrado) {
+        print("AJUSTE DE INTERVALOS (POR TENER MENOS DE 5 VALORES)")
+        
+        var contsBien:[Float] = Array()
+        var intervalosBien:[(Float,Float)] = Array()
+        
+        var i:Int = 0
+        
+        while (i < conts.count) {
+          if (conts[i] < 5){
+            var temp = conts[i+1] + conts[i]
+            contsBien.append(temp)
+            intervalosBien.append((intervalos[i].0,intervalos[i+1].1))
+            i += 1
+          }
+          else{
+            contsBien.append(conts[i])
+            intervalosBien.append((intervalos[i].0,intervalos[i].1))
+          }
+          i += 1
+        }
+        
+        print("---Intervalos---")
+        for i in 0..<intervalosBien.count {
+          print("\(intervalosBien[i].0) - \(intervalosBien[i].1)")
+          print("Cantidad de nums en el intervalo = \(Int(contsBien[i]))")
+        }
+        
+        K = Float(contsBien.count)
+        
+        Ei = (1.0/K) * 100.0
+        
+        for i in 0..<contsBien.count {
+          X2 += (pow(contsBien[i] - Ei, 2)) / Ei
+        }
+        
+      }
+      else {
+        print("NO SE SUFRIÓ AJUSTE DE INTERVALOS (POR LA CANTIDAD DE VALORES QUE CONTIENEN)")
+        print("---Intervalos---")
+        for i in 0..<intervalos.count {
+          print("\(intervalos[i].0) - \(intervalos[i].1)")
+          print("Cantidad de nums en el intervalo = \(Int(conts[i]))")
+        }
+        
+        Ei = (1.0/K) * 100.0
+        
+        for i in 0..<conts.count {
+          X2 += (pow(conts[i] - Ei, 2)) / Ei
+        }
+        
+      }
+      
+      print("\nN = \(numeros.count)")
+      print("K = \(Int(K))")
+      print("X2 = \(X2)")
+      
     }
     else {
       print("ERROR: Revisar la cantidad de numeros en cada intervalo")
