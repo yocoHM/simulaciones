b8d920678a45571c408a68a7218a401a7119a6e3
yocoHM <yocoyanihernandez@gmail.com>
2016-10-21 11:15:14 -0500

    cambios en ColasMM1

--- colas/ColasMM1.playground/Contents.swift
+++ colas/ColasMM1.playground/Contents.swift
@@ -1,8 +1,11 @@
 import UIKit
 
-let lambda:Float = 4.0
-let mew:Float = 5.0
+let lambda:Float = 3.0
+let mew:Float = 8.0
 let k:Float = 4.0
+let m:Float = 2.0
+let Cs:Float = 6.0
+let Cw:Float = 10.0
 
 var L:Float = lambda / (mew - lambda)
 var W:Float = 1 / (mew - lambda)
@@ -12,6 +15,8 @@ var Ro:Float = lambda / mew
 var P0:Float = 1 - (lambda / mew)
 var Pnk:Float = powf((lambda / mew), k+1.0)
 
+var Ct:Float = m*Cs + lambda*W*Cw
+
 print("L = \(L)")
 print("W = \(W)")
 print("Lq = \(Lq)")
@@ -19,3 +24,4 @@ print("Wq = \(Wq)")
 print("Ro = \(Ro)")
 print("P0 = \(P0)")
 print("P(n>k) = \(Pnk) donde k = \(k)")
+print("Ct = \(Ct)")
